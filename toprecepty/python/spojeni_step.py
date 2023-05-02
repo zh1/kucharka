764512a9adea476f92bc2356433b37bf91df85b2
Verutynka <128778990+Verutynka@users.noreply.github.com>
2023-05-02 10:27:27 +0200

    Add files via upload

---
+++ toprecepty/python/spojeni_step.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+concatenated_df = pd.DataFrame()
+
+for i in range(0, 18):
+    
+    input_file = f'step{i}.csv'
+
+    input_df = pd.read_csv(input_file)
+    
+    concatenated_df = pd.concat([concatenated_df, input_df], ignore_index=True)
+
+concatenated_df.to_csv('all_step.csv', index=True)
